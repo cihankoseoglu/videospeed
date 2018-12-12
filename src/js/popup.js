5f58cf4e611e1e0de7ef068558f060619e25d9f0
Cihan Koseoglu <cihan@cihankoseoglu.com>
2018-12-12 04:11:49 +0300

    Bind slider value to playback rate text

--- src/js/popup.js
+++ src/js/popup.js
@@ -22,7 +22,9 @@
 
 
 
-const slider = document.getElementById('slider');
+const sliderEl = document.querySelector('#slider');
+const playbackRateEl = document.querySelector('.playback-rate');
+
 const defaultPlaybackRate = 1.0;
 const largeValues = [0.1, 1.0, 2.0, 5.0];
 
@@ -30,28 +32,16 @@ const filterSmallValues = (value, type) => {
     return !!largeValues.includes(value) ? 1 : 0
 }
 
-noUiSlider.create(slider, {
+noUiSlider.create(sliderEl, {
     range: {
         'min': [0.1, 0.1],
         '30%': [1.0, 0.1],
         '60%': [2.0, 0.5],
         'max': [5.0]
     },
-
     start: defaultPlaybackRate,
-
-
-    // Put '0' at the bottom of the slider
-    orientation: 'horizontal',
-
-    // Move handle on tap, bars are draggable
     behaviour: 'tap-drag',
     tooltips: true,
-    format: {
-
-    }
-
-    // Show a scale with the slider
     pips: {
         mode: 'steps',
         density: 20,
@@ -62,11 +52,16 @@ noUiSlider.create(slider, {
 
 // Set the slider value to default playback rate
 document.querySelector('.button-reset').addEventListener('click', () => {
-    slider.noUiSlider.set(defaultPlaybackRate);
+    sliderEl.noUiSlider.set(defaultPlaybackRate);
 
     //set storage value to 1
     //set video speed to 1
 
 });
+
+sliderEl.noUiSlider.on('update', () => {
+    playbackRateEl.textContent = `Videos playing at ${sliderEl.noUiSlider.get()}x speed.`
+})
+
 
 
