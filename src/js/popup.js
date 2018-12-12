5d15cecaec55ef6da7f759deb796d5044c65ecc5
Cihan Koseoglu <cihan@cihankoseoglu.com>
2018-12-12 03:45:18 +0300

    Add noUiSlider to the project, add existing css to a main.css file, initialize slider

--- src/js/popup.js
+++ src/js/popup.js
@@ -23,18 +23,44 @@
 
 
 const slider = document.getElementById('slider');
+const defaultPlaybackRate = 1.0;
+const filterSmallValues = (value, type) => {
+    if (type == 0) {
+        return value / 1 == 1 ? 1 : -1
+    }
+
+}
 
 noUiSlider.create(slider, {
-    start: [1.00],
     range: {
-        'min': [0.1],
+        'min': [0.1, 0.1],
+        '30%': [1.0, 0.1],
+        '60%': [2.0, 0.5],
         'max': [5.0]
+    },
+
+    start: defaultPlaybackRate,
+
+
+    // Put '0' at the bottom of the slider
+    orientation: 'horizontal',
+
+    // Move handle on tap, bars are draggable
+    behaviour: 'tap-drag',
+    tooltips: true,
+
+    // Show a scale with the slider
+    pips: {
+        mode: 'steps',
+        density: 20,
+        filter: filterSmallValues
+
     }
 });
 
-// Set the slider value to 1.00
+// Set the slider value to default playback rate
 document.querySelector('.button-reset').addEventListener('click', () => {
-    slider.noUiSlider.set(1.00);
+    slider.noUiSlider.set(defaultPlaybackRate);
 
     //set storage value to 1
     //set video speed to 1
