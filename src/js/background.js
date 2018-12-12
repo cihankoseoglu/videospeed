0750a9b555acba9a08198a98f6a8d8ab2c85293d
Cihan Koseoglu <cihan@cihankoseoglu.com>
2018-12-12 04:37:26 +0300

    Refactor manifest.json, background.js and remove options code

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,7 @@
+/**
+ * Uses declarativeContent to check whether the page is Netflix or YouTube
+ */
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({color: '#3aa757'}, () => {
-    console.log('The color is green.');
-  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [
